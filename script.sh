8c4791c028288001cc14801c42f2a61a0a126024
jagunbabs <jtunz02@gmail.com>
2020-09-03 12:23:17 +0100

    my 3rd module

--- script.sh
+++ script.sh
@@ -19,3 +19,16 @@ school='LASU'
 founded='1983'
 
 echo My name is $name I studied $course at Lagos State university $school which was founded $founded
+
+read -p "Enter name: " name 
+
+read -p "Enter school: " school
+
+read -p "Enter course: " course
+
+echo My name is $name my school is $school and i studied $course
+
+read -p "Username for https://github.com: " username
+read -sp "Password for https://"$username"@github.com: " password
+
+echo "this is the" $username and $password
