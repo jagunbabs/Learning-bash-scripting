1a281f9fbf762e1444b3d6d760b5c6559ba45c32
jagunbabs <jtunz02@gmail.com>
2020-09-03 11:14:49 +0100

    Please enter the commit message for your changes. Lines starting

--- script.sh
+++ script.sh
@@ -1,2 +1,21 @@
 echo Hello world
 
+echo This is my current working directory $PWD
+
+echo This is my Bash $BASH
+
+echo This is my Home $HOME
+
+echo This is my bash version $BASH_VERSION
+
+echo Ihis is my path $PATH
+
+name='Tunde'
+
+course='physics'
+
+school='LASU'
+
+founded='1983'
+
+echo My name is $name I studied $course at Lagos State university $school which was founded $founded
